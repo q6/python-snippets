564d4850a9d0995c1058125d5065577ba247667c
Mayor Oxen <larmannjan@gmail.com>
2015-03-21 11:02:36 +0100

    Updated cyhper to allow fo variable offsets

--- cypher.py
+++ cypher.py
@@ -1,23 +1,27 @@
+import cProfile
 
-s = 'The Date is 12 may 20 year 4715. King of the Castle.'
+s = 'If you can read this you have cracked the code piano stairs'
 
-def cypher(s, offset):
+def cypher(s, offset_lst):
     s = list(s.upper())
+    num_offsets = len(offset_lst)
     letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890.'
     len_letters = len(letters)
     for index, char in enumerate(s):
-       char_index = letters.find(char) + offset 
-       char_index = char_index % len_letters
-       s[index] = letters[char_index] 
+        offset = offset_lst[index % num_offsets]
+        char_index = letters.find(char) + offset 
+        char_index = char_index % len_letters
+        s[index] = letters[char_index] 
     return ''.join(s)
 
 
 letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890.'
+print(len(letters))
 # n = int(input('Enter an int for the offset:\n'))
-n = 12
+n = [24, 3, 19, 36, 33, 5, 22]
 s = cypher(s, n)
 print(s)
-
-s = cypher(s, len(letters)-n)
+n = [len(letters)-o for o in n]
+s = cypher(s, n)
 print(s)
 
