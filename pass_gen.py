12e0162c9e02fc48aac9c40d327c9b759820751c
Jan Larmann <larmannjan@gmail.com>
2016-04-16 10:55:53 -0500

    Fixed bug where exclude characters were still in password.

--- pass_gen.py
+++ pass_gen.py
@@ -2,6 +2,7 @@
 # user can determine length, characters, exclude characters, how many passwords to generate
 
 from random import choice
+from re import sub
 
 # character the password can be made up off
 chars_numbers = '1234567890'
@@ -59,12 +60,24 @@ def generate_password_set(user_char_sets):
 def ask_user_exclude_characters():
     # ask the user if they want to exclude any characters
     chars_exclude = input('\nAre there any character you want to exclude? (If no, press Enter.)\n:')
+    chars_exclude = ''.join(set(chars_exclude))  # remove dupes from str
     return chars_exclude
 
 
 def take_out_exclude_characters(password_set, exclude_characters):
     # take out the chars the user wants to exclude
-    set_for_password = password_set.strip(exclude_characters)
+    # set_for_password = sub(r'{}'.format(exclude_characters), '', password_set)
+    new_set = ''
+    for c in password_set:
+        if c in exclude_characters:
+            pass
+        else:
+            if c not in new_set:
+                new_set += c
+    return new_set
+
+    print('*'*20)
+    print(set_for_password)
     return set_for_password
 
 
@@ -76,7 +89,7 @@ def generate_passwords(password_numbers, length, set_for_password):
         password = ''
         # generate one password
         for i in range(length):
-            password += choice(set_for_password)  # TODO find out if this will be on one line. might no include \n
+            password += choice(set_for_password)
         passwords.append(password)
     return '\n'.join(passwords)
 
@@ -87,14 +100,14 @@ def ask_user_parameters_and_generate():
     user_desired_password_sets = ask_user_char_sets()
     password_sets = generate_password_set(user_desired_password_sets)
     exclude_characters = ask_user_exclude_characters()
-    password_sets = take_out_exclude_characters(password_sets, exclude_characters)
-    generated_passwords = generate_passwords(password_number, password_length, password_sets)
+    password_sets_2 = take_out_exclude_characters(password_sets, exclude_characters)
+    generated_passwords = generate_passwords(password_number, password_length, password_sets_2)
 
     print('Welcome to the password generator.')
     print('\n', '='*10, 'GENERATED PASSWORD', '='*10, '\n', sep='')
     print(generated_passwords)
     print('\n', '='*38, sep='')
-    print('\npassword generated from this set:', password_sets, sep='\n')
+    print('\npassword generated from this set:', password_sets_2, sep='\n')
     print('\nThe following characters were excluded:', exclude_characters, sep='\n')
 
 
